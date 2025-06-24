56d2cd9660763bdcf769d133c37fb987c7fcc0cb
strykerin <dacosta.pereirafabio@gmail.com>
2025-06-24 17:23:32 -0300

    finish custom univeresal signer

--- core-sdk-functions/custom-universal-signer/index.ts
+++ core-sdk-functions/custom-universal-signer/index.ts
@@ -1,26 +1,27 @@
 // Full Documentation: https://push.org/docs/chain/build/advanced/custom-universal-signer/
 
 // Import Push Chain Core
-import { PushChain } from '@pushchain/core'
+import { PushChain } from '@pushchain/core';
 
 // Import if you are using ethers
-import { ethers, hexlify, getBytes } from 'ethers'
-import { hexToBytes } from 'viem'
+import { ethers, hexlify, getBytes } from 'ethers';
+import { hexToBytes } from 'viem';
 
 // Import input
-import readline from 'readline'
+import readline from 'readline';
+import { UniversalSigner } from '@pushchain/core/src/lib/universal/universal.types';
 
 // ⭐️ MAIN FUNCTION ⭐️
 async function main() {
   // ETHERS USAGE
-  console.log("🚀 Let's create custom universal signer")
+  console.log("🚀 Let's create custom universal signer");
   console.log(
     "⚠️  You ONLY DO THIS when you don't have a supported library like ethers, viem, etc. or want to create a custom implementation"
-  )
-  console.log('💡 If you have a supported library, check createUniversalSigner example')
+  );
+  console.log('💡 If you have a supported library, check createUniversalSigner example');
 
-  console.log('------')
-  console.log('🛠️  We will create a custom universal signer using ethers for this example')
+  console.log('------');
+  console.log('🛠️  We will create a custom universal signer using ethers for this example');
 
   // We need to pass the following to PushChain.utils.signer.construct(account, {options})
   // 1. account which is a universal account
@@ -29,115 +30,115 @@ async function main() {
   // 2.2 signMessage
   // 2.3 signTypedData
 
-  console.log('------\n\n')
-  console.log('1️⃣  Converting account to universal account...')
+  console.log('------\n\n');
+  console.log('1️⃣  Converting account to universal account...');
 
   // 1. account to universal account
   // Create random wallet
-  const provider = new ethers.JsonRpcProvider('https://sepolia.drpc.org')
-  const wallet = ethers.Wallet.createRandom(provider)
+  const provider = new ethers.JsonRpcProvider('https://sepolia.drpc.org');
+  const wallet = ethers.Wallet.createRandom(provider);
 
   // Convert wallet.address to Universal Account
   // change chain to ETHEREUM_SEPOLIA or SOLANA_DEVNET to see universal transaction
   const universalAccount = PushChain.utils.account.toUniversal(wallet.address, {
     chain: PushChain.CONSTANTS.CHAIN.ETHEREUM_SEPOLIA,
-  })
-  console.log('✅ Created Universal Account:\n', JSON.stringify(universalAccount, null, 2), '\n\n\n')
+  });
+  console.log('✅ Created Universal Account:\n', JSON.stringify(universalAccount, null, 2), '\n\n\n');
 
   // 2. options to construct
   // 2.1 signAndSendTransaction
   // create custom Sign and Send Transaction
-  console.log('2️⃣  Creating signing functions for custom signer')
-  const customSignAndSendTransaction = async (unsignedTx) => {
-    const unsignedHex = hexlify(unsignedTx)
-    const tx = ethers.Transaction.from(unsignedHex)
-    const txResponse = await wallet.sendTransaction(tx)
-    return hexToBytes(txResponse.hash)
-  }
-  console.log('✅  Created customSignAndSendTransaction function', customSignAndSendTransaction)
+  console.log('2️⃣  Creating signing functions for custom signer');
+  const customSignAndSendTransaction = async (unsignedTx: any) => {
+    const unsignedHex = hexlify(unsignedTx);
+    const tx = ethers.Transaction.from(unsignedHex);
+    const txResponse = await wallet.sendTransaction(tx);
+    return hexToBytes(txResponse.hash as `0x${string}`);
+  };
+  console.log('✅  Created customSignAndSendTransaction function', customSignAndSendTransaction);
 
   // 2.2 signMessage
-  const customSignMessage = async (message) => {
+  const customSignMessage = async (message: any) => {
     // Sign message using ethers wallet
-    const signature = await wallet.signMessage(message)
+    const signature = await wallet.signMessage(message);
 
     // Always a Uint8Array
-    return Uint8Array.from(signature)
-  }
-  console.log('✅  Created customSignMessage function', customSignMessage)
+    return Uint8Array.from(signature);
+  };
+  console.log('✅  Created customSignMessage function', customSignMessage);
 
   // 2.3 signMessage
-  const customSignTypedData = async ({ domain, types, primaryType, message }) => {
-    const sigHex = await wallet.signTypedData(domain, types, message)
-    return getBytes(sigHex)
-  }
+  const customSignTypedData = async ({ domain, types, primaryType, message }: any) => {
+    const sigHex = await wallet.signTypedData(domain, types, message);
+    return getBytes(sigHex);
+  };
   // This is optional as solana doesn't support sign typed data
-  console.log('✅  Created customSignTypedData function', customSignTypedData, '\n\n\n')
+  console.log('✅  Created customSignTypedData function', customSignTypedData, '\n\n\n');
 
-  console.log('3️⃣  Creating Universal Signer Skeleton with custom signing functions...')
+  console.log('3️⃣  Creating Universal Signer Skeleton with custom signing functions...');
   // * Construct the universal signer skeleton with custom signing functions
   const universalSignerSkeleton = PushChain.utils.signer.construct(universalAccount, {
     signAndSendTransaction: customSignAndSendTransaction,
     signMessage: customSignMessage,
     signTypedData: customSignTypedData,
-  })
+  });
   console.log(
     '✅  Created Universal Signer with custom signing functions\n',
     JSON.stringify(universalSignerSkeleton, null, 2),
     '\n\n\n'
-  )
+  );
 
-  console.log('4️⃣  Creating Universal Signer from Skeleton...')
+  console.log('4️⃣  Creating Universal Signer from Skeleton...');
   // ** Pass constructed universal signer skeleton to create universal signer **
-  const universalSigner = await PushChain.utils.signer.toUniversal(universalSignerSkeleton)
-  console.log('✅  Created Universal Signer Skeleton\n', JSON.stringify(universalSigner, null, 2), '\n\n\n')
+  const universalSigner = await PushChain.utils.signer.toUniversal(universalSignerSkeleton);
+  console.log('✅  Created Universal Signer Skeleton\n', JSON.stringify(universalSigner, null, 2), '\n\n\n');
 
   // ** Optional: Initialize Push Chain Client and Send Transaction **
-  await optionalPushChainClientAndSendTx(universalSigner)
+  await optionalPushChainClientAndSendTx(universalSigner);
 }
-await main().catch(console.error)
+main().catch(console.error);
 
 // ** Optiona: Initialize Push Chain Client and Send Transaction **
-async function optionalPushChainClientAndSendTx(universalSigner) {
+async function optionalPushChainClientAndSendTx(universalSigner: UniversalSigner) {
   // setup for input
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-  })
+  });
 
-  console.log('5️⃣  (Optional) Initializing Push Chain Client with custom signer')
+  console.log('5️⃣  (Optional) Initializing Push Chain Client with custom signer');
   // ** Initialize Push Chain client **
   const pushChainClient = await PushChain.initialize(universalSigner, {
     network: PushChain.CONSTANTS.PUSH_NETWORK.TESTNET,
-  })
+  });
   // JSON.stringify with BigInt support
   console.log(
     '✅  Push Chain Client Initialized\n',
     JSON.stringify(pushChainClient, (_, v) => (typeof v === 'bigint' ? v.toString() : v)),
     '\n\n\n'
-  )
+  );
 
   // Wrap in a promise to handle async/await
-  await new Promise((resolve) => {
+  await new Promise<void>((resolve) => {
     rl.question(
       `💰 Please make sure that this wallet address: ${pushChainClient.universal.origin.address} is funded with test tokens for this chain ${pushChainClient.universal.origin.chain}\n⏎  Press Enter to continue...`,
       async () => {
         try {
-          console.log('6️⃣  (Optional) Sending transaction...')
+          console.log('6️⃣  (Optional) Sending transaction...');
 
           // Send Transaction
           const tx = await pushChainClient.universal.sendTransaction({
             to: '0x0000000000000000000000000000000000042101',
             value: BigInt(1),
-          })
-          console.log('📤 Transaction sent:', tx)
+          });
+          console.log('📤 Transaction sent:', tx);
         } catch (err) {
-          console.error('❌ Error sending transaction:', err)
+          console.error('❌ Error sending transaction:', err);
         } finally {
-          rl.close()
-          resolve()
+          rl.close();
+          resolve();
         }
       }
-    )
-  })
+    );
+  });
 }
