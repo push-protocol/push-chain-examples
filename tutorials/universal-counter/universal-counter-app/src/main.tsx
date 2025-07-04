1c47a8642f5f0624df5fb57bd3f02fc0bde592f3
harshrajat <harsh@epns.io>
2025-07-04 16:56:50 +0400

    feat: integrate Push UI kit and refactor UniversalCounter contract with UEA support

--- tutorials/universal-counter/universal-counter-app/src/main.tsx
+++ tutorials/universal-counter/universal-counter-app/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import { PushUI, PushUniversalWalletProvider } from '@pushchain/ui-kit'
+
 import App from './App.tsx'
+import './index.css'
+
+  // Define Wallet Config
+  const walletConfig = {
+    network: PushUI.CONSTANTS.PUSH_NETWORK.TESTNET,
+  };
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PushUniversalWalletProvider config={walletConfig}>
+      <App />
+    </PushUniversalWalletProvider>
   </StrictMode>,
 )
