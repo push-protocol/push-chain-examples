de960f2399c33c4ee191884c7427ed73d2350ab9
harshrajat <harsh@epns.io>
2025-07-05 04:47:03 +0400

    chore: update vite base path for push-chain app deployment

--- tutorials/universal-counter/app/vite.config.js
+++ tutorials/universal-counter/app/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Use relative paths
+  base: './push-chain-ballsy-app-deployment',  // Use relative paths
   resolve: {
     alias: {
       process: 'process/browser',
